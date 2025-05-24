6a0ff854a5b835817a1fe62c6bc755c6d2c01ce3
KL-ML <knataly0513@gmail.com>
2025-05-24 13:58:09 +0300

    Update Logo.jsx

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,9 +1,9 @@
 import PropTypes from 'prop-types';
-
+import css from './Logo.module.css'
 export default function Logo({ logoImgPath }) {
   return (
     <>
-      <a href="./index.html" aria-label="Site logo">
+      <a className={css.logo} href="./index.html" aria-label="Site logo">
         <img src={logoImgPath} alt="GoIt logo" width="100" height="30" />
       </a>
     </>
