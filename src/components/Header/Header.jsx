e4a3f57b4a4af85f9834a2e17bf1d3e8c514e524
KL-ML <knataly0513@gmail.com>
2025-05-24 13:57:35 +0300

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,16 +3,23 @@ import PropTypes from 'prop-types';
 import HeaderTitle from '../HeaderTitle/HeaderTitle';
 import Logo from '../Logo/Logo';
 import css from './Header.module.css';
+import SearchBar from '../SearchBar/SearchBar';
 
 export default function Header({
   logoImgPath,
   onUpdate,
   moduleNumber,
   moduleTitle,
+  onSearch,
+  isDisabled,
 }) {
   return (
     <header className={css.header}>
-      <Logo logoImgPath={logoImgPath} />
+      <div className={css.searchGroup}>
+        <Logo logoImgPath={logoImgPath} />
+        <SearchBar onSearch={onSearch} isDisabled={isDisabled} />
+      </div>
+
       <HeaderTitle
         moduleNumber={moduleNumber}
         moduleTitle={moduleTitle}
