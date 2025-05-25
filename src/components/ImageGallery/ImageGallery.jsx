0eecb2c61751ebae46d23d24e23d8f3ec9892bf2
KL-ML <knataly0513@gmail.com>
2025-05-25 15:13:50 +0300

    Update ImageGallery.jsx

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,28 +1,24 @@
+import PropTypes from 'prop-types';
 import ImageCard from '../ImageCard/ImageCard';
-import Button from '../Button/Button';
+import css from './ImageGallery.module.css';
 
 export default function ImageGallery({ items, openModal }) {
   return (
     <>
       {items.length > 0 && (
-        <>
-          <ul>
-            {items.map(photo => (
-              <li key={photo.id}>
-                <ImageCard photo={photo} openModal={openModal} />
-              </li>
-            ))}
-          </ul>
-          {/* {total > page &&
-            <Button
-              text="Load more ..."
-              variant="filled"
-              btnType="button"
-              handleLoadMoreClick={handleLoadMoreClick}
-            />
-          } */}
-        </>
+        <ul className={css.gallery}>
+          {items.map(photo => (
+            <li key={photo.id}>
+              <ImageCard photo={photo} openModal={openModal} />
+            </li>
+          ))}
+        </ul>
       )}
     </>
   );
 }
+
+ImageGallery.PropTypes = {
+  items: PropTypes.object,
+  openModal: PropTypes.func,
+};
