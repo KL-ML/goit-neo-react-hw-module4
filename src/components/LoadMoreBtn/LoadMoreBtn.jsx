4adc9485eaaea4db9590228aadfc1f76296ce4fb
KL-ML <knataly0513@gmail.com>
2025-05-25 15:14:01 +0300

    Update LoadMoreBtn.jsx

--- src/components/LoadMoreBtn/LoadMoreBtn.jsx
+++ src/components/LoadMoreBtn/LoadMoreBtn.jsx
@@ -1,7 +1,30 @@
-export default function LoadMoreBtn() {
-    return (
-      <>
-        <div>LoadMoreBtn</div>
-      </>
-    );
+import PropTypes from 'prop-types';
+import Button from '../Button/Button';
+
+export default function LoadMoreBtn({
+  text,
+  variant,
+  btnType,
+  handleLoadMoreClick,
+  positionVariant,
+}) {
+  return (
+    <>
+      <Button
+        text={text}
+        variant={variant}
+        btnType={btnType}
+        handleLoadMoreClick={handleLoadMoreClick}
+        positionVariant={positionVariant}
+      />
+    </>
+  );
 }
+
+LoadMoreBtn.PropTypes = {
+  text: PropTypes.string,
+  variant: PropTypes.string,
+  btnType: PropTypes.string,
+  handleLoadMoreClick: PropTypes.func,
+  positionVariant: PropTypes.string,
+};
